a192afa61a570d35d111316a4fd05be0e8d33a85
Davron <d.mirabdullayev@mail.ru>
2022-11-07 16:40:38 +0500

    finish style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -11,39 +10,58 @@ import "./styles.css";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
-import { data } from "./mocData";
+import {data} from './mocData'
 
 export default function App() {
   return (
-    <div className="_wrapper">
-      <h1 style={{ textAlign: "center" }}>Swiper Slider</h1>
+    <div className="wrap">
       <Swiper
-        // spaceBetween={30}
+        spaceBetween={30}
         loop={true}
+        lazy={true}
+        centeredSlides={true}
         autoplay={{
-          delay: 3000,
+          delay: 5000,
           disableOnInteraction: false,
+         
         }}
+       
         pagination={{
           clickable: true,
         }}
         navigation={true}
-
-        keyboard={{
-          enabled: true,
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
         }}
-
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        {data.map((item, index) => {
+        {data.map((item,index)=> {
           return (
             <SwiperSlide key={index}>
               <img src={item.url} alt={item.alt} />
             </SwiperSlide>
           )
         })}
-
+        {/* <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
       </Swiper>
     </div>
   );
